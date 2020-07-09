4681183b945b45e272d071be71b9912c682b6167
NareAmiryan <67760359+NareAmiryan@users.noreply.github.com>
2020-07-09 12:24:12 +0400

    task_2 commit

---
+++ task_2.js
@@ -0,0 +1,13 @@
+let a = 15;
+let b = 8;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+ 
+alert(sum);
+alert(difference);
+alert(product);
+alert(quotient);
+alert(remainder);
